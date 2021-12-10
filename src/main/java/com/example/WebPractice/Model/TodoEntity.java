21e9230bcb8ad756fb62346292e4b3f568d10bab
조승현 <jorealbusiness@gmail.com>
2021-12-10 18:36:36 +0900

    레이어드 아키텍쳐 구현
    정리:
    Persistance -> 데이터베이스와 통신하며 필요한 쿼리를 보내고 해석해 엔티티 오브젝트로 변환해 보내준다.
    
    Service -> HTTP나 데이터베이스 같은외부 컨포넌트로부터 추상화돼 비즈니스 로직에만 집중할 수 있다.
    
    Controller -> 주로 HTTP 요청과 응답을 어떻게 넘겨받고 리턴하느냐, 즉 외부 세계와 통신하는 규약을 정의한다.

--- src/main/java/com/example/WebPractice/Model/TodoEntity.java
+++ src/main/java/com/example/WebPractice/Model/TodoEntity.java
@@ -1,4 +1,10 @@
 package com.example.WebPractice.Model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +19,16 @@ import lombok.NoArgsConstructor;
 //클래스 내의 모든 멤버변수를 매개변수로 받는 생성자를 만들어 준다.
 @Data
 //getter, setter 메서드를 만들어 준다.
+@Entity
+@Table(name = "todo")
+//이 엔티티는 데이터베이스의 todo 테이블에 매핑된다는 뜻이다.
 public class TodoEntity {
+ @Id //기본 키가 될 필드에 지정한다.
+ @GeneratedValue(generator = "system-uuid")
+ //generator ="" 어떤 방식으로 ID를 생성할지 지정한다.
+ //system-uuid -> Hibernate가 제공하는 기본 Generator가 아닌 나만의 Generator를 사용하는 경우 이용한다.
+ @GenericGenerator(name="system-uuid", strategy = "uuid")
+ // uuid를 사용하는 system-uuid라는 이름의 GenericGenerator를 만들고, @GeneratedValue가 참조해 사용한다.
  private String id; //이 오브젝트의 아이디
  private String userId; // 이 오브젝트를 생성한 사용자의 아이디
  private String title; // Todo 타이틀(예 : 운동하기)
