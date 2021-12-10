61b5a99f40d7925bbdd7a9bf8fbfc4792691bf82
조승현 <jorealbusiness@gmail.com>
2021-12-10 22:11:47 +0900

    Revert "레이어드 아키텍쳐 구현"
    This reverts commit 21e9230bcb8ad756fb62346292e4b3f568d10bab.

--- src/main/java/com/example/WebPractice/service/TodoService.java
+++ src/main/java/com/example/WebPractice/service/TodoService.java
@@ -1,24 +1,11 @@
 package com.example.WebPractice.service;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.example.WebPractice.Model.TodoEntity;
-import com.example.WebPractice.persisance.TodoRepository;
 
 @Service
 // 비즈니스 로직을 수행하는 서비스 레이어임을 알려준다.
 public class TodoService {
 	
-	@Autowired
-	private TodoRepository repository;
-	
 	public String testService() {
-		// TodoEntity 생성
-		TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
-		// TodoEntity 저장
-		repository.save(entity);
-		// TodoEntity 검색
-		TodoEntity savedEntity = repository.findById(entity.getId()).get();
-		return savedEntity.getTitle();
+		return "Test Service";
 	}
 }
