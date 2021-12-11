cfaab08aab52dcd5da1bddbbddeb5a49a03c392b
조승현 <jorealbusiness@gmail.com>
2021-12-11 18:48:23 +0900

    Update Todo 구현

--- src/main/java/com/example/WebPractice/service/TodoService.java
+++ src/main/java/com/example/WebPractice/service/TodoService.java
@@ -1,8 +1,11 @@
 package com.example.WebPractice.service;
+import com.sun.tools.javac.comp.Todo;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
+
 import com.example.WebPractice.Model.TodoEntity;
 import com.example.WebPractice.persistence.TodoRepository;
 
@@ -50,5 +53,27 @@ public class TodoService {
 			log.warn("Unknown user");
 			throw new RuntimeException("Unknown user");
 		}
+	}
+
+	public List<TodoEntity> update(final TodoEntity entity) {
+		//1. 저장할 엔티티가 유효한지 확인한다. 이 메서드는 2.3.1 Create Todo에서 구현했다.
+		valiate(entity);
+
+		//2. 넘겨받은 엔티티 id를 이용해 TodoEntity를 가져온다.
+		final Optional<TodoEntity> original = repository.findById(entity.getId());
+
+		if(original.isPresent())
+		//3. 반환된 TodoEntity가 존재하면 값을 새 entity 값으로 덮어 씌운다.
+		{
+			final TodoEntity todo = original.get();
+			todo.setTitle(entity.getTitle());
+			todo.setDone(entity.getDone());
+
+			//4. 데이터베이스에 새 값을 저장한다.
+			repository.save(todo);
+		}
+			//5.retrieve메서드를 이용해 사용자의 모든 Todo 리스트를 리턴한다.
+			return retrieve(entity.getUserId());
+
 	}
 }
